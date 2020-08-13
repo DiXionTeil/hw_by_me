398ac0bd752228a250bd2028f596d9709690fb07
Matzo_pythoner <hi.milobrozzers@gmail.com>
2020-08-13 16:17:03 +0300

    corrected

--- Matzo_python/in_work/Yur_Bra_hw15.py
+++ Matzo_python/in_work/Yur_Bra_hw15.py
@@ -8,83 +8,97 @@ from math import sqrt
 
 
 class Figures:
-    def __init__(self, side_1, side_2=0):
+    def __init__(self, side_1):
         self.side_1 = side_1
-        self.side_2 = side_2
+
+    @property
+    def side_1(self):
+        return self._side_1
+
+    @side_1.setter
+    def side_1(self, s_1):
+        if s_1 > 0:
+            self._side_1 = s_1
+        else:
+            raise ValueError
 
 
 class Triangle(Figures):
-    def area(self):
-        if self.side_2 != 0:
-            # прямоугольный треугольник
-            return (self.side_1 + self.side_2) / 2
-        # равносторонний треугольник
-        return round(sqrt(3) / 4 * pow(self.side_1, 2), 3)
+    def __init__(self, side_1, side_2=0):
+        super().__init__(side_1)
+        self.side_2 = side_2
 
-    def perimeter(self):
-        if self.side_2 != 0:
-            # прямоугольный треугольник
-            return round(sqrt(pow(self.side_1, 2) + pow(self.side_2, 2)) + self.side_1 + self.side_2, 3)
-        # равносторонний треугольник
+    def area_rectangular(self):
+        return self.side_1 * self.side_2 // 2
+
+    def area_isosceles(self):
+        return round(self.side_1 * self.side_2, 2)
+
+    def area_equilateral(self):
+        return round(pow(self.side_1, 2) * sqrt(3) / 4, 2)
+
+    def perimeter_rectangular(self):
+        return round(sqrt(pow(self.side_1, 2) + pow(self.side_2, 2)) + self.side_1 + self.side_2, 2)
+
+    def perimeter_isosceles(self):
+        return round(2 * (sqrt(pow(self.side_1, 2) + pow(self.side_2, 2)) + self.side_2), 2)
+
+    def perimeter_equilateral(self):
         return self.side_1 * 3
 
 
 class Square(Figures):
     def area(self):
-        if self.side_2 != 0:
-            raise ValueError
         return pow(self.side_1, 2)
 
     def perimeter(self):
-        if self.side_2 != 0:
-            raise ValueError
         return self.side_1 * 4
 
 
 class Rectangle(Figures):
+    def __init__(self, side_1, side_2):
+        super().__init__(side_1)
+        self.side_2 = side_2
+
+    @property
+    def side_2(self):
+        return self._side_2
+
+    @side_2.setter
+    def side_2(self, s_2):
+        if s_2 > 0:
+            self._side_2 = s_2
+        else:
+            raise ValueError
+
     def area(self):
-        if self.side_2 != 0:
-            return self.side_1 * self.side_2
-        raise ValueError
+        return self.side_1 * self.side_2
 
     def perimeter(self):
-        if self.side_2 != 0:
-            return self.side_1 * 2 + self.side_2 * 2
-        raise ValueError
+        return self.side_1 * 2 + self.side_2 * 2
 
 
 # ────────── * TESTING * ──────────
 
 # триугольник
-triangle1 = Triangle(5, 6)
-print(triangle1.area())
-print(triangle1.perimeter())
-triangle2 = Triangle(8)
-print(triangle2.area())
-print(triangle2.perimeter())
+triangle1 = Triangle(5, 10)
+print(triangle1.area_rectangular())
+print(triangle1.area_isosceles())
+print(triangle1.area_equilateral())
+print(triangle1.perimeter_rectangular())
+print(triangle1.perimeter_isosceles())
+print(triangle1.perimeter_equilateral())
 print('─' * 10)
 
 # квадрат
-# ошибки
-# square1 = Square(5, 6)
-# print(square1.area())
-# print(square1.perimeter())
-
-# норм
-square2 = Square(8)
+square2 = Square(2)
 print(square2.area())
 print(square2.perimeter())
 print('─' * 10)
 
 # прямоугольник
-# норм
-rectangle1 = Rectangle(5, 6)
+rectangle1 = Rectangle(5, 3)
 print(rectangle1.area())
 print(rectangle1.perimeter())
-
-# ошибки
-# rectangle2 = Rectangle(8)
-# print(rectangle2.area())
-# print(rectangle2.perimeter())
 
 # ─────────────────────────────────
