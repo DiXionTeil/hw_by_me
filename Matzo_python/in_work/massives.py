3da3d23f1ca15adeacaf1956b4be0ae9c155ed8b
Matzo_pythoner <hi.milobrozzers@gmail.com>
2020-09-01 15:20:34 +0300

    modified

--- Matzo_python/in_work/massives.py
+++ Matzo_python/in_work/massives.py
@@ -1,4 +1,4 @@
-''''''
+"""arrays examples"""
 '''
 ─────── <  1  > ───────
 построить функцию, кот возвращает массив в виде строки (str):
@@ -12,10 +12,12 @@
     n+1  n+2 ... N
 
 '''
+
+
 def massive_min_max(min_val: int, n: int):
-    def massive_n(n, min_val):
-        mass_n = f'{min_val}'
-        for i in range(min_val + 1, n + 1):
+    def massive_n(n, min_value):
+        mass_n = f'{min_value}'
+        for i in range(min_value + 1, n + 1):
             mass_n += '\t' + ''.join(str(i))
         return mass_n
 
@@ -36,7 +38,7 @@ def massive_min_max(min_val: int, n: int):
 '''
 ─────── <  2  > ───────
 построить функцию, кот возвращает массив в виде строки (str):
-от |N_min| до N_max, например:
+от |N_min| до N_max каждую i-ю строку + на 1, например:
 от |-2| до n ↓
 
     2  1  0  1 ...  n
@@ -46,15 +48,19 @@ def massive_min_max(min_val: int, n: int):
     n  n+1 n+2 .... N
 
 '''
+
+
 def massive_cro_min_max(min_val: int, n: int):
-    def massive_n(n, min_val):
-        mass_n = f'{abs(min_val)}'
-        # ─────── <  если через 1  > ───────
-        mass_n = f'{abs(min_val) * 2}'
-        for i in range(min_val + 1, n + 1):
+    def massive_n(n, min_value):
+        mass_n = f'{abs(min_value)}'
+        # ───── <  если через 1  > ─────
+        # mass_n = f'{abs(min_val) * 2}'
+        # ──────────────────────────────
+        for i in range(min_value + 1, n + 1):
             mass_n += '\t' + ''.join(str(abs(i)))
-            # ─────── <  если через 1  > ───────
+            # ───────── <  если через 1  > ────────────
             # mass_n += '\t' + ''.join(str(abs(i * 2)))
+            # ─────────────────────────────────────────
         return mass_n
 
     mass = ''
@@ -71,16 +77,38 @@ def massive_cro_min_max(min_val: int, n: int):
 # print(massive_cro_min_max(-2, 5))
 
 # ─────────────────────────────────
+# TODO ─────── <  3  > ───────
 '''
 ─────── <  3  > ───────
 построить функцию, кот возвращает массив в виде строки (str):
-от |N_min| до N_max, например:
+от |N_min| до N_max каждую i-ю строку + на 1, например:
 от |-1| до n ↓
 
     1  0  1  2 ...  n
-    0  1  2 ... n - 1
-    1  0  1 ... n - 2
+    0  1  2 ... n + 1
+    1  2  3 ... n + 2
     .................
-    n  n-1 n-2 .... 1
+    n  n+1 n+2 .... N
 
 '''
+
+
+def massive_abs_min_max(min_val: int, n: int):
+    def massive_n(n, min_value):
+        mass_n = f'{abs(min_value)}'
+        for i in range(min_value + 1, n + 1):
+            mass_n += '\t' + ''.join(str(abs(i)))
+        return mass_n
+
+    mass = ''
+    for i in range(min_val, n + 1):
+        mass += massive_n(n, min_val) + '\n'
+        n += 1
+        min_val += 1
+    return mass
+
+# ────────── * TESTING * ──────────
+
+# print(massive_abs_min_max(-5, 5))
+
+# ─────────────────────────────────
