a4055dc649b6a0488903e27014e0ccc661553b2f
Matzo_pythoner <hi.milobrozzers@gmail.com>
2020-09-01 06:40:05 +0300

    modifications

--- Matzo_python/in_work/massives.py
+++ Matzo_python/in_work/massives.py
@@ -49,8 +49,12 @@ def massive_min_max(min_val: int, n: int):
 def massive_cro_min_max(min_val: int, n: int):
     def massive_n(n, min_val):
         mass_n = f'{abs(min_val)}'
+        # ─────── <  если через 1  > ───────
+        mass_n = f'{abs(min_val) * 2}'
         for i in range(min_val + 1, n + 1):
             mass_n += '\t' + ''.join(str(abs(i)))
+            # ─────── <  если через 1  > ───────
+            # mass_n += '\t' + ''.join(str(abs(i * 2)))
         return mass_n
 
     mass = ''
@@ -63,8 +67,8 @@ def massive_cro_min_max(min_val: int, n: int):
 
 # ────────── * TESTING * ──────────
 
-print(massive_cro_min_max(0, 4))
-print(massive_cro_min_max(-2, 5))
+# print(massive_cro_min_max(0, 4))
+# print(massive_cro_min_max(-2, 5))
 
 # ─────────────────────────────────
 '''
