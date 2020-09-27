231305ee97b4016bcdf07738a10c4761a184b2e9
Matzo_pythoner <hi.milobrozzers@gmail.com>
2020-09-27 23:49:55 +0300

    done

--- Matzo_python/in_work/Labirint.py
+++ Matzo_python/in_work/Labirint.py
@@ -60,63 +60,73 @@ def game(labyrinth_str):
     for _ in labyrinth_true:
         print(_)
 
-    enter_points_1 = []  # точка входа
-    enter_points_2 = []  # точка выхода
+    points_edge = []
 
     # проверка точек вхождения в верху лабиринта
-    for i in labyrinth_true[0]:
-        if '.' in labyrinth_true[0]:
-            enter_points_1.append(0)  # строка
-            enter_points_1.append(labyrinth_true[0].index('.'))  # столбец
-        if labyrinth_true[0].count('.') > 1:
-            labyrinth_true[0].reverse()
-            if '.' in labyrinth_true[0]:
-                enter_points_2.append(0)  # строка
-                enter_points_2.append(labyrinth_true[0].index('.'))  # столбец
-            labyrinth_true[0].reverse()
+    if labyrinth_true[0].count('.') > 0:
+        points_edge.append(0)  # строка
+        points_edge.append(labyrinth_true[0].index('.'))  # столбец
+    if labyrinth_true[0].count('.') > 1:
+        labyrinth_true[0].reverse()
+        points_edge.append(0)  # строка
+        points_edge.append(labyrinth_true[0].index('.'))  # столбец
+        labyrinth_true[0].reverse()
 
     # проверка точек вхождения по сторонам лабиринта
+    left_edge = []
+    right_edge = []
     for i in labyrinth_true:
-        # слева лабиринта
-        if '.' in labyrinth_true[labyrinth_true.index(i)][0]:
-            if not enter_points_1:  # если точка входа не найдена ранее
-                enter_points_1.append(labyrinth_true.index(i))
-                enter_points_1.append(i.index('.'))
-            elif enter_points_1 and not enter_points_2:  # если точка входа уже известна
-                enter_points_2.append(labyrinth_true.index(i))
-                enter_points_2.append(i.index('.'))
+        left_edge.append(i[0])
+        right_edge.append(i[-1])
+
+    # слева лабиринта
+    if left_edge.count('.') > 0:
+        if '.' in left_edge:
+            # print(left_edge)
+            points_edge.append(left_edge.index('.'))
+            points_edge.append(0)
+    if left_edge.count('.') > 1:
+        left_edge.reverse()
+        points_edge.append(len(left_edge) - 1 - left_edge.index('.'))
+        points_edge.append(0)
+
         # справа лабиринта
-        if '.' in labyrinth_true[labyrinth_true.index(i)][-1]:
-            if not enter_points_1:  # если точка входа не найдена ранее
-                enter_points_1.append(labyrinth_true.index(i))
-                enter_points_1.append(i.index('.'))
-            elif enter_points_1 and not enter_points_2:  # если точка входа уже известна
-                enter_points_2.append(labyrinth_true.index(i))
-                enter_points_2.append(i.index('.'))
+        if right_edge.count('.') > 0:
+            if '.' in right_edge:
+                # print(left_edge)
+                points_edge.append(right_edge.index('.'))
+                points_edge.append(0)
+        if right_edge.count('.') > 1:
+            right_edge.reverse()
+            points_edge.append(len(right_edge) - 1 - right_edge.index('.'))
+            points_edge.append(0)
 
     # проверка точек вхождения снизу лабиринта
-    for i in labyrinth_true[-1]:
-        if '.' in labyrinth_true[-1]:
-            if not enter_points_1:
-                enter_points_1.append(len(labyrinth_true) - 1)  # строка
-                enter_points_1.append(labyrinth_true[-1].index('.'))  # столбец
-            elif enter_points_1 and not enter_points_2:
-                enter_points_2.append(len(labyrinth_true) - 1)  # строка
-                enter_points_2.append(labyrinth_true[-1].index('.'))  # столбец
-        if labyrinth_true[-1].count('.') > 1 and not enter_points_2:
-            labyrinth_true[-1].reverse()
-            if '.' in labyrinth_true[-1]:
-                enter_points_2.append(len(labyrinth_true) - 1)  # строка
-                enter_points_2.append(labyrinth_true[-1].index('.'))  # столбец
-            labyrinth_true[-1].reverse()
-    return f'Точка входа: {enter_points_1}\nТочка выхода: {enter_points_2}'
+    if labyrinth_true[-1].count('.') > 0:
+        points_edge.append(len(labyrinth_true) - 1)  # строка
+        points_edge.append(labyrinth_true[-1].index('.'))  # столбец
+    if labyrinth_true[-1].count('.') > 1:
+        labyrinth_true[-1].reverse()
+        points_edge.append(len(labyrinth_true) - 1)  # строка
+        points_edge.append(labyrinth_true[-1].index('.'))  # столбец
+        labyrinth_true[-1].reverse()
+
+    return f'Точка входа_1: {[points_edge[0], points_edge[1]]}\nТочка входа_2: {[points_edge[2], points_edge[3]]}'
 
 
 if __name__ == "__main__":
     # условно, это строка, преобразованная из матрици:
-    labyrinth_str = '##########\n.........#\n######.###\n#......###\n' \
+    labyrinth_str_1 = '##########\n.........#\n######.###\n#......###\n' \
                     '#.####.###\n#........#\n##.#######\n##.##.####\n' \
                     '##......##\n#######.##'
 
-    print(game(labyrinth_str))
+    labyrinth_str_2 = '##########\n.......###\n######.###\n#......###\n' \
+                      '#.####.###\n.......###\n##.#######\n##.#######\n' \
+                      '##....####\n##########'
+
+    labyrinth_str_3 = '######.###\n.......###\n######.###\n#......###\n' \
+                      '#.####.###\n#......###\n##.#######\n##.#######\n' \
+                      '##....####\n##########'
+
+    print(game(labyrinth_str_3))
 
